d6804d8dca989f63322164b7f68833cf6964aa22
Nishant Jawla <40868641+NishantJawla@users.noreply.github.com>
2021-01-22 02:25:45 -0800

    Created a login layout section and set up the control for using the arrow keys

--- MAIN.cpp
+++ MAIN.cpp
@@ -26,6 +26,191 @@ void bo(int a,int b)
 	rectangle(4*mx/5,4*my/5+60,5*mx/5+50,4*my/5+100);
 	setcolor(WHITE);
 }
+void setb(int i)
+{	switch(i)
+	{case 0:bo(YELLOW,WHITE);
+		break;
+	 case 1:bo(WHITE,YELLOW);
+		break;
+	}
+}
+
+int sc()
+{
+	int dir;
+	int c=0;
+
+	setb(c);
+	while(1)
+	{ dir=getch();
+		if(dir==80)
+		{	c++;
+			if(c==2)
+			{c=0;}
+		}
+		if(dir==72)
+		{c--;
+			if(c==-1)
+			{c=1;}
+		}
+		if(dir==13)
+			{break;}
+		setb(c);
+	}//while
+
+return(c);
+}
+void login_layout()
+{       int lab;
+	lab:
+
+	cleardevice();
+	setbkcolor(RED);
+	setcolor(YELLOW);
+	int mx=getmaxx();
+	int my=getmaxy();
+	settextstyle(1,0,3);
+	outtextxy(mx/3-10,10,"CARD DETAIL");
+	setcolor(WHITE);
+	settextstyle(0,0,1);
+	outtextxy(mx/20,my/3+5,"Card Number");
+	setcolor(YELLOW);
+	rectangle(mx/20*5,my/3-20+5,mx/20*15-5,my/3+20+5);
+	setcolor(WHITE);
+	outtextxy(mx/20,my/2,"Enter PIN");
+	rectangle(mx/20*5,my/2-20,mx/20*15-5,my/2+20);
+	char cno[21],di[2];
+	int a;int bm=0;
+	a:
+	int i=0;
+	di[1]='\0';
+	//int l1;l1:
+	//int x=23,y=11;
+	for(i=0;i<20;i++)
+	{cno[i]='\0';}
+	i=0;
+	while(i<20)
+	{       bm=0;
+		 cno[i]=getch();
+		if(cno[i]==13)
+		{
+		  if(i<19)
+		  { outtextxy(mx/20*6+10,my/3+30,"cardno.not upto size ");
+		    delay(500); bm=1;
+		    setfillstyle(SOLID_FILL,RED);
+		    bar(mx/20*6+10,my/3+25+5,mx/20*6+250,my/3+40);
+		  --i;}
+		  else
+		  break;
+		}
+
+		if(cno[i]==8)
+		{
+			//di[0]=' ';
+			--i;
+			if(i<0)
+				i=0;
+			setcolor(RED);
+		       //	outtextxy(mx/20*6+(i*10),my/3,di);
+		     for(int j=0;j<20;j++)
+				rectangle(mx/20*6+(i*10)-3,my/3-5,mx/20*6+(i*10)-3+j,my/3-5+j);
+			setcolor(WHITE);
+			continue;
+		}
+		if((cno[i]<48||cno[i]>57)&&cno[i]!=13)
+		{	outtextxy(mx/20*6+10,my/3+30+5,"Enter valid Info.");
+			delay(500);
+			setfillstyle(SOLID_FILL,RED);
+			bar(mx/20*6,my/3-10,mx/20*15-20,my/3+10);
+
+			bar(mx/20*6+10,my/3+25+5,mx/20*6+200,my/3+40+5);
+			goto a;
+			bm=1;
+		}
+		if(bm==0)
+		{	di[0]=cno[i];
+		outtextxy(mx/20*6+(i*10),my/3+3,di); }
+
+		i++;
+	}//Card no. while ends
+
+	delay(500);
+	//flush();  ******************************************
+	setcolor(YELLOW);
+	rectangle(mx/20*5,my/2-20,mx/20*15-5,my/2+20);
+	setcolor(WHITE);
+	rectangle(mx/20*5,my/3-20+5,mx/20*15-5,my/3+20+5);
+	int b;
+	b:
+	char ipin[5]; ipin[4]='\0';
+	int i2=0;
+	while(i2<4)
+	{	 ipin[i2]=getch();
+		if(ipin[i2]==13)
+		  {	if(i2<4)
+		  { outtextxy(mx/20*6+10,my/2+130,"pin not upto size ");
+		    delay(400);
+		    setfillstyle(SOLID_FILL,RED);
+		    bar(mx/20*6,my/2+120,mx/20*6+300,my/2+170);
+		    bar(mx/20*5+2,my/2-10,mx/20*15-30,my/2+10);
+		  i2=0;
+		  continue;
+		  }
+		  else
+		  break; }
+		if(ipin[i2]==8)
+		{       i2--;
+			if(i2<0)
+				i2=0;
+			setcolor(RED);
+
+			for(int j=0;j<20;j++)
+				rectangle(mx/4+10+(i2*10)-3,my/2-3,mx/20*6+(i2*10)-3+j,my/2-3+j);
+			setcolor(WHITE);
+			continue;
+
+		}
+		if(ipin[i2]<48||ipin[i2]>57)
+		{
+			outtextxy(mx/4+20,my/2+50,"Enter valid Info.");
+			delay(500);
+			setfillstyle(SOLID_FILL,RED);
+			bar(mx/4+10,my/2-3,mx/20*15-20,my/2+5);
+			bar(mx/4+20,my/2+45,mx/20*15+200,my/2+60);
+			goto b;
+		}//if ends
+		else
+		{		outtextxy(mx/4+10+(i2*10),my/2,"* ");
+		}
+		//to match the ipin with text file
+		i2++;
+	}  //end of while
+	rectangle(4*mx/5,4*my/5,5*mx/5+50,4*my/5+40);
+	rectangle(4*mx/5,4*my/5+60,5*mx/5+50,4*my/5+100);
+	outtextxy(4*mx/5+10,4*my/5+20,"Proceed");
+	outtextxy(4*mx/5+10,4*my/5+80,"Cancel");
+
+	cno[20]='\0';
+	setcolor(WHITE);
+	rectangle(mx/20*5,my/2-20,mx/20*15-5,my/2+20);
+      int lm=sc();
+      if(lm==1)
+      goto lab;
+
+     lm=verify(cno,ipin);
+      if(lm==0)
+	{ outtextxy(mx/3,my-100,"No such account exists");
+	 delay(600);
+       goto lab;
+	}
+      if(lm==1)
+      {welcome();
+       //cout<<global.cno<<" "<<global.name<<" "<<global.opal;
+      main_menu();
+      }
+
+getche();
+}//ends login_layout
 
 void main()
 {
